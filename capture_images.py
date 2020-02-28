fce07cfde0d5a2e6f4036a442ecf5053cd698c88
Hernando Castano <castano.ha@gmail.com>
2020-02-28 19:53:29 +0000

    Capture photos quickly from one camera

--- capture_images.py
+++ capture_images.py
@@ -1,3 +1,4 @@
+import io
 from time import sleep
 from time import time
 from picamera import PiCamera
@@ -15,6 +16,7 @@ def init_gpios():
     # GPIO.output(channel_list[0], 0)
     # GPIO.output(channel_list[1], 0)
 
+# https://raspberrypi.stackexchange.com/questions/22040/take-images-in-a-short-time-using-the-raspberry-pi-camera-module#22110
 def init_camera():
     # Want to make sure we start with Camera A
     with SMBus(1) as bus:
@@ -24,7 +26,8 @@ def init_camera():
 
     global camera
     camera = PiCamera()
-    camera.resolution = (1024, 768) # Check resolution
+    camera.resolution = (640, 480)
+    camera.framerate = 80
     sleep(2)
 
 # Will take photos from both cameras "simultaneously"
@@ -32,11 +35,26 @@ def take_photos():
     start_time = time()
     flip_to_camera('A')
     camera.capture('testing_images/foo1.jpg')
+    end_time = time()
 
     flip_to_camera('B')
     camera.capture('testing_images/foo2.jpg')
-    end_time = time()
     print ("Took %s" % (end_time - start_time))
+
+# https://picamera.readthedocs.io/en/release-1.3/recipes2.html#rapid-capture-and-processing
+def filenames(frames):
+    frame = 0
+    while frame < frames:
+        yield 'testing_images/image%02d.jpg' % frame
+        frame += 1
+
+def take_n_photos(n):
+    # Set up 40 in-memory streams
+    start = time()
+    camera.capture_sequence(filenames(n), use_video_port=True)
+    finish = time()
+    # How fast were we?
+    print('Captured 40 images at %.2ffps' % (n / (finish - start)))
 
 def flip_to_camera(cam):
     with SMBus(1) as bus:
@@ -53,7 +71,8 @@ def flip_to_camera(cam):
 
 init_gpios()
 init_camera()
-take_photos()
+# take_photos()
+take_n_photos(10)
 
 # Cleanup channels that we've used
 GPIO.cleanup()

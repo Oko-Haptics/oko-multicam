13e008531051144c092e19ea89c1e4c90bebba2b
Hernando Castano <castano.ha@gmail.com>
2020-03-10 20:14:13 -0400

    Use worker thread to take photos

--- MultiCam/multi_cam.py
+++ MultiCam/multi_cam.py
@@ -23,14 +23,12 @@ class MultiCam:
         self.right_photo = 0 # None
         self.left_photo = 0 #None
 
-        self.i2c_addr = "0x70"
-        self.channel_list = [4, 17]
-
-        self._init_gpios()
-        self._init_camera()
-
+        # Exit runs twice with this, need to
+        # figure out how to get clean exits from
+        # failures and normal shutdown
         atexit.register(self._cleanup)
 
+        print(self)
         # Starts background thread dedicated to taking images
         self.processor = CaptureImages(self, self.camera)
 
@@ -46,16 +44,60 @@ class MultiCam:
         print(f"Get right photo {self.right_photo}")
         return self.right_photo
 
-    def take_photo(self):
-        self.camera.capture('testing_images/multicam.jpg')
+    def _switch_camera(self):
+        print("Switching camera")
+        data = int(self.curr_camera) + 1
+        print(f"Data {data}")
+        self._write_i2c(data)
+        print(f"GPIO Output / Input {GPIO.input(self.channel_list[0])}")
+        GPIO.output(self.channel_list[0], not GPIO.input(self.channel_list[0]))
+        print("Curr Camera")
+        self.curr_camera = not self.curr_camera
+        sleep(0.2)
 
-    def take_photos(self, n):
-        print("Taking photos")
-        self.camera.capture('testing_images/multicam1.jpg')
+    def _cleanup(self):
+        # self.camera.close()
+        # GPIO.cleanup()
+        print(f'Pre-Cleanup Termination {self.processor.terminated}')
+        self.processor.terminated = True
+        print(f'Post-Cleanup Termination {self.processor.terminated}')
+        self.processor.join()
 
-        self._write_i2c(2)
-        GPIO.output(self.channel_list[0], GPIO.HIGH)
-        self.camera.capture('testing_images/multicam2.jpg')
+class CaptureImages(threading.Thread):
+    def __init__(self, owner, camera):
+        print("Init Capture Images")
+        super(CaptureImages, self).__init__()
+
+        self.terminated = False
+        self.owner = owner # Owner is the MultiCam library
+
+        self.camera = None
+        self.i2c_addr = "0x70"
+        self.channel_list = [4, 17]
+
+        self._init_gpios()
+        self._init_camera()
+
+        self.start() # Will call the run method
+
+    # The thread stops being alive when the run()
+    # method returns (intentionally or not)
+    def run(self):
+        while not self.terminated:
+            self.owner.left_photo += 1
+            self._use_camera('A')
+            f_left = 'testing_images/multithread_L_%02d.jpg' % self.owner.left_photo
+            self.camera.capture(f_left)
+            sleep(1)
+
+            self.owner.right_photo += 1
+            self._use_camera('B')
+            f_right = 'testing_images/multithread_R_%02d.jpg' % self.owner.right_photo
+            self.camera.capture(f_right)
+            sleep(1)
+
+        print("He ded")
+        self._cleanup()
 
     # https://raspberrypi.stackexchange.com/questions/22040/take-images-in-a-short-time-using-the-raspberry-pi-camera-module#22110
     def _init_camera(self):
@@ -91,26 +133,21 @@ class MultiCam:
                     print('Error with I2C write, retrying...')
                     sleep(0.5 * retries)
                     retries += 1
+        sleep(0.1)
 
-    def _switch_camera(self):
-        print("Switching camera")
-        data = int(self.curr_camera) + 1
-        print(f"Data {data}")
-        self._write_i2c(data)
-        print(f"GPIO Output / Input {GPIO.input(self.channel_list[0])}")
-        GPIO.output(self.channel_list[0], not GPIO.input(self.channel_list[0]))
-        print("Curr Camera")
-        self.curr_camera = not self.curr_camera
-        sleep(0.2)
-
-    # Useful for using a specific camera
     def _use_camera(self, cam):
         if cam == 'A':
-            write_i2c(data=1)
-            GPIO.output(channel_list[0], GPIO.LOW)
+            self._write_i2c(data=1)
+            print("Flipping GPIO A")
+            print(f"GPIO Mode A {GPIO.getmode()}")
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(self.channel_list, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.output(self.channel_list[0], GPIO.LOW)
         elif cam == 'B':
-            write_i2c(data=2)
-            GPIO.output(channel_list[0], GPIO.HIGH)
+            self._write_i2c(data=2)
+            print("Flipping GPIO B")
+            print(f"GPIO Mode B {GPIO.getmode()}")
+            GPIO.output(self.channel_list[0], GPIO.HIGH)
         else:
             print("Wrong camera setting")
         sleep(0.1)
@@ -118,107 +155,3 @@ class MultiCam:
     def _cleanup(self):
         self.camera.close()
         GPIO.cleanup()
-
-        self.processor.terminated = True
-        self.processor.join()
-
-class CaptureImages(threading.Thread):
-    def __init__(self, owner, camera):
-        print("Init Capture Images")
-        super(CaptureImages, self).__init__()
-
-        self.terminated = False
-        self.camera = camera
-        self.owner = owner # Owner is the MultiCam library
-
-        print(f"Init Owner Left: {self.owner.left_photo}")
-        print(f"Init Owner Right: {self.owner.right_photo}")
-
-        self.start() # Will call the run method
-
-    def run(self):
-        while not self.terminated:
-            self.owner.left_photo += 1
-            # sleep(1)
-            self.owner.right_photo += 1
-            sleep(1)
-
-            # image_L = self.camera.capture()
-            # self.owner.left = image_L
-            # MultiCam.left = image_L
-            # # Maybe have a delay here
-            # image_R = self.camera.capture()
-            # MultiCam.right = image_R
-            # self.owner.right = image_R
-
-# Will take photos from both cameras "simultaneously"
-def take_photos():
-    start_time = time()
-    switch_cam(GPIO.LOW, 1)
-    camera.capture('testing_images/foo1.jpg')
-    end_time = time()
-
-    switch_cam(GPIO.HIGH, 2)
-    camera.capture('testing_images/foo2.jpg')
-    print ("Took %s" % (end_time - start_time))
-
-def take_photos_stream():
-    temp_streamA = io.BytesIO()
-    temp_streamB = io.BytesIO()
-
-    start_time = time()
-    switch_cam(GPIO.LOW, 1)
-    camera.capture(temp_streamA, 'jpeg')
-
-    switch_cam(GPIO.HIGH, 2)
-    camera.capture(temp_streamB, 'jpeg')
-    end_time = time()
-    print ("Stream Took %s" % (end_time - start_time))
-    print(f'{str(temp_streamA) == str(temp_streamB)}')
-
-def switch_cam(gpio, i2c):
-    sleep(0.2)
-    GPIO.output(channel_list[0], gpio)
-    write_i2c(i2c)
-
-# https://picamera.readthedocs.io/en/release-1.3/recipes2.html#rapid-capture-and-processing
-def filenames(frames):
-    frame = 0
-    while frame < frames:
-        yield gimme_file(frame)
-        frame += 1
-        print(f'Updating frame {frame}')
-
-def gimme_file(frame):
-    if frame % 2 == 0:
-        switch_cam(GPIO.LOW, 1)
-    else:
-        switch_cam(GPIO.HIGH, 2)
-    return 'testing_images/image%02d.jpg' % frame
-
-def take_n_photos(n):
-    print('Starting to take photos')
-    # Set up 40 in-memory streams
-    start = time()
-    camera.capture_sequence(filenames(n), use_video_port=False)
-    finish = time()
-    # How fast were we?
-    print('Captured %d images at %.2ffps' % (n / (finish - start), n + 1))
-
-def test_i2c_writes(writes):
-    count = 0
-    for i in range(0, writes):
-        try:
-            write_i2c(i % 2 + 1)
-            count+=1
-            sleep(0.5)
-        except IOError:
-            print('Error at cycle %d' % count)
-
-# init_gpios()
-# init_camera()
-# take_photos()
-# take_photos_stream()
-# take_n_photos(40)
-# test_i2c_writes(40)
-# cleanup()

54cf2f74dfc439e5984bf14240c112bd81424f7b
Hernando Castano <castano.ha@gmail.com>
2020-03-12 20:07:45 -0400

    Take photos for consumption by OpenCV
    This leverages picamera's PiRGBArray in order
    to take photos as numpy arrays which can then
    be understood by OpenCV. Right now it's only
    taking one pair photos but this should change
    soon.

--- MultiCam/multi_cam.py
+++ MultiCam/multi_cam.py
@@ -1,11 +1,13 @@
 import atexit
 import io
 import threading
+import RPi.GPIO as GPIO
+from cv2
 from time import sleep
 from time import time
 from picamera import PiCamera
+from picamera.array import PiRGBArray
 from smbus2 import SMBus
-import RPi.GPIO as GPIO
 
 # Might need a custom `write` impl here
 # But that would only be used if the caller
@@ -20,13 +22,13 @@ class MultiCam:
         # 'A' => False
         # 'B' => True
         self.curr_camera = False
-        self.right_photo = 0 # None
-        self.left_photo = 0 #None
+        self.right_photo = io.BytesIO()
+        self.left_photo = io.BytesIO()
 
         # Exit runs twice with this, need to
         # figure out how to get clean exits from
         # failures and normal shutdown
-        atexit.register(self._cleanup)
+        atexit.register(self.cleanup)
 
         print(self)
         # Starts background thread dedicated to taking images
@@ -35,27 +37,16 @@ class MultiCam:
     # Get the most recently taken photo
     # from the left camera
     def get_left_photo(self):
-        print(f"Get left photo {self.left_photo}")
+        # print(f"Get left photo")
         return self.left_photo
 
     # Get the most recently taken photo
     # from the right camera
     def get_right_photo(self):
-        print(f"Get right photo {self.right_photo}")
+        # print(f"Get right photo")
         return self.right_photo
 
-    def _switch_camera(self):
-        print("Switching camera")
-        data = int(self.curr_camera) + 1
-        print(f"Data {data}")
-        self._write_i2c(data)
-        print(f"GPIO Output / Input {GPIO.input(self.channel_list[0])}")
-        GPIO.output(self.channel_list[0], not GPIO.input(self.channel_list[0]))
-        print("Curr Camera")
-        self.curr_camera = not self.curr_camera
-        sleep(0.2)
-
-    def _cleanup(self):
+    def cleanup(self):
         # self.camera.close()
         # GPIO.cleanup()
         print(f'Pre-Cleanup Termination {self.processor.terminated}')
@@ -75,27 +66,51 @@ class CaptureImages(threading.Thread):
         self.i2c_addr = "0x70"
         self.channel_list = [4, 17]
 
+        self.right_count = 0;
+        self.left_count = 0;
+
         self._init_gpios()
         self._init_camera()
 
-        self.start() # Will call the run method
+        self.raw_output_left = PiRGBArray(self.camera)
+        self.raw_output_right = PiRGBArray(self.camera)
+
+        atexit.register(self._cleanup)
+
+        self.start()
 
     # The thread stops being alive when the run()
     # method returns (intentionally or not)
     def run(self):
+
+        start = time()
         while not self.terminated:
-            self.owner.left_photo += 1
+            self.left_count += 1
             self._use_camera('A')
-            f_left = 'testing_images/multithread_L_%02d.jpg' % self.owner.left_photo
-            self.camera.capture(f_left)
-            sleep(1)
 
-            self.owner.right_photo += 1
+            # OpenCV uses the 'bgr'
+            # f_left = 'testing_images/multithread_L_%02d.jpg' % self.owner.left_photo
+            # self.camera.capture(f_left)
+            self.camera.capture(self.raw_output_left, format='bgr')
+            print('Captured Left %dx%d image' % (
+                self.raw_output_left.array.shape[1], self.raw_output_left.array.shape[0]))
+            imwrite('./testing_images/left_cv2.jpg', self.raw_output_left.array)
+
+
+            self.right_count += 1
             self._use_camera('B')
-            f_right = 'testing_images/multithread_R_%02d.jpg' % self.owner.right_photo
-            self.camera.capture(f_right)
-            sleep(1)
+            # f_right = 'testing_images/multithread_R_%02d.jpg' % self.right_count
+            self.camera.capture(self.raw_output_right, format='bgr')
+            print('Captured Right %dx%d image' % (
+                self.raw_output_right.array.shape[1], self.raw_output_right.array.shape[0]))
+            imwrite('./testing_images/right_cv2.jpg', self.raw_output_right.array)
 
+            # TODO: Remove
+            break;
+
+        end = time()
+        delta = end - start
+        print(f'Took {self.right_count} {self.left_count} photos in {delta} seconds')
         print("He ded")
         self._cleanup()
 
@@ -138,20 +153,26 @@ class CaptureImages(threading.Thread):
     def _use_camera(self, cam):
         if cam == 'A':
             self._write_i2c(data=1)
-            print("Flipping GPIO A")
-            print(f"GPIO Mode A {GPIO.getmode()}")
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(self.channel_list, GPIO.OUT, initial=GPIO.LOW)
             GPIO.output(self.channel_list[0], GPIO.LOW)
         elif cam == 'B':
             self._write_i2c(data=2)
-            print("Flipping GPIO B")
-            print(f"GPIO Mode B {GPIO.getmode()}")
             GPIO.output(self.channel_list[0], GPIO.HIGH)
         else:
             print("Wrong camera setting")
         sleep(0.1)
 
     def _cleanup(self):
+        print("Cleaning up camera and GPIOs")
         self.camera.close()
         GPIO.cleanup()
+
+    def _switch_camera(self):
+        print("Switching camera")
+        data = int(self.curr_camera) + 1
+        print(f"Data {data}")
+        self._write_i2c(data)
+        print(f"GPIO Output / Input {GPIO.input(self.channel_list[0])}")
+        GPIO.output(self.channel_list[0], not GPIO.input(self.channel_list[0]))
+        print("Curr Camera")
+        self.curr_camera = not self.curr_camera
+        sleep(0.2)
